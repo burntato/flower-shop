ed1c97c27753af739ac496cdb45fa0fe0b8ac43b
burntato <kineticz300@gmail.com>
2022-03-11 14:04:20 +0700

    fix database + sync

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Featured;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index() {
-        return view('welcome.home');
+        $featured = Featured::all();
+        return view('welcome.home', compact('featured'));
     }
 }
--- resources/views/welcome/home.blade.php
+++ resources/views/welcome/home.blade.php
@@ -3,18 +3,34 @@
 @section('title', 'Flower Shop - Home')
 
 @section('content')
-    {{-- Product --}}
     <div class="title"><span class="title_icon"><img src="{{ asset('images/bullet1.gif') }}"
                 alt="" /></span>Featured products</div>
+    {{-- Product --}}
+    @forelse ($featured as $item)
+        <div class="feat_prod_box">
+            <div class="prod_img"><a href="#"><img src="{{ asset('images/prod1.gif') }}" alt="" border="0" /></a>
+            </div>
+            <div class="prod_det_box">
+                <div class="box_top"></div>
+                <div class="box_center">
+                    <div class="prod_title">{{$item->title}}</div>
+                    <p class="details">{{$item->desc}}</p>
+                    <a href="#" class="more">- more details -</a>
+                    <div class="clear"></div>
+                </div>
+                <div class="box_bottom"></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    @empty
     <div class="feat_prod_box">
-        <div class="prod_img"><a href="#"><img src="{{ asset('images/prod1.gif') }}" alt="" border="0" /></a></div>
+        <div class="prod_img"><a href="#"><img src="{{ asset('images/prod1.gif') }}" alt="" border="0" /></a>
+        </div>
         <div class="prod_det_box">
             <div class="box_top"></div>
             <div class="box_center">
-                <div class="prod_title">Product name</div>
-                <p class="details">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation.</p>
+                <div class="prod_title">Dummy Title</div>
+                <p class="details">Dummy Desc</p>
                 <a href="#" class="more">- more details -</a>
                 <div class="clear"></div>
             </div>
@@ -22,22 +38,9 @@
         </div>
         <div class="clear"></div>
     </div>
-    <div class="feat_prod_box">
-        <div class="prod_img"><a href="#"><img src="{{ asset('images/prod2.gif') }}" alt="" border="0" /></a></div>
-        <div class="prod_det_box">
-            <div class="box_top"></div>
-            <div class="box_center">
-                <div class="prod_title">Product name</div>
-                <p class="details">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation.</p>
-                <a href="#" class="more">- more details -</a>
-                <div class="clear"></div>
-            </div>
-            <div class="box_bottom"></div>
-        </div>
-        <div class="clear"></div>
-    </div>
+    @endforelse
+    {{-- Product --}}
+
     {{-- Product end --}}
     <div class="title"><span class="title_icon"><img src="{{ asset('images/bullet2.gif') }}"
                 alt="" /></span>New products</div>
